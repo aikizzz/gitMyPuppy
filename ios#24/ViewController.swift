481358a7a2c5fbc6c038a76384523776272bd3eb
aikizzz <jlpss92051@gmail.com>
2023-05-23 20:02:06 +0800

    Initial Commit

---
+++ ios#24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios#24
+//
+//  Created by Hsin Zhang on 2023/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
